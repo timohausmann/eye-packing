d6560f1ab5976b11f522ccdec05edc65433d3b79
Timo Hausmann <hello.timohausmann@gmail.com>
2026-05-01 20:07:43 +0200

    add biome

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import './style.css'
+import './style.css';
 import p5 from 'p5';
 
 export const sketch = (p: p5) => {
-    p.setup = () => {
-        p.createCanvas(400, 400);
-    }
+  p.setup = () => {
+    p.createCanvas(400, 400);
+  };
 
-    p.draw = () => {
-        p.background(220);
-        p.ellipse(50,50,80,80);
-    }
-}
+  p.draw = () => {
+    p.background(220);
+    p.ellipse(50, 50, 80, 80);
+  };
+};
 
 const appDiv = document.querySelector('#app');
-if(appDiv) {
-    new p5(sketch, appDiv as HTMLElement);
+if (appDiv) {
+  new p5(sketch, appDiv as HTMLElement);
 }
